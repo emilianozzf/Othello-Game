efb2cb34d6f2c198d44e36ead2146f2e6e3497c0
朱振方 <zhuzhenfang@Jerrys-MacBook-Pro.local>
2019-09-09 16:20:29 -0700

    Completes homework 1

---
+++ hw01/student_info.py
@@ -0,0 +1,11 @@
+my_name = 'Emiliano Zhu'
+my_birthday = '07/03/1995'
+my_hobbies = 'music and sport'
+my_favorite_book = '<The Economist>'
+my_favorite_movie = '<Inception>'
+
+print ('My name is ' + my_name + '.')
+print ('My birthday is ' + my_birthday + '.')
+print ('My hobbies are ' + my_hobbies + '.')
+print ('My favorite book is ' + my_favorite_book + '.')
+print ('My favorite movie is ' + my_favorite_movie + '.')
