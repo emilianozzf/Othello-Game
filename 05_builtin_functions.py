2f947d9c4157782bc05ca213ce9a30d1997603a1
朱振方 <zhuzhenfang@Jerrys-MacBook-Pro.local>
2019-10-02 21:44:13 -0700

    finished my lab05

--- 05_builtin_functions.py
+++
@@ -1,8 +0,0 @@
-def hello_function(argument):
-    """Prints a hello message with a value
-    String -> None"""
-    print("Hello,", argument)
-
-print(dir(str))  # returns a list of methods that can be called
-print(help(hello_function))  # returns a document of a function
-eval(input("Tell me what to do: "))
---
+++ lab05/word_count.py
@@ -0,0 +1,27 @@
+def main():
+    filename = input("Enter the file name: ")
+    try:
+        f = open(filename, "r")
+    except:
+        print("Can't open", filename)
+        return
+    num_words = 0
+    num_nonwhite_chrs = 0
+    num_alphanumeric_chrs = 0
+
+    for line in f:
+        words = line.strip().split(' ')
+        if words[0] != '':
+            num_words += len(words)
+        nonwhite_chrs = str.join('', words)
+        num_nonwhite_chrs += len(nonwhite_chrs)
+        for c in nonwhite_chrs:
+            if (ord(c.lower()) >= ord('a') and ord(c.lower()) <= ord('z')) \
+               or (ord(c) >= ord('0') and ord(c) <= ord('9')):
+                num_alphanumeric_chrs += 1
+
+    print("Words:", num_words)
+    print("Characters: ", num_nonwhite_chrs)
+    print("Letters & numbers:", num_alphanumeric_chrs)
+
+main()
