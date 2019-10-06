4d4ecf87961e91598cc85375b2a0f461ebc6c81f
朱振方 <zhuzhenfang@Jerrys-MacBook-Pro.local>
2019-10-05 17:31:11 -0700

    modified my lab05 for the 1st time

--- lab05/word_count.py
+++ lab05/word_count.py
@@ -4,27 +4,42 @@
 
 
 def main():
+    # Gets the file name from the user
     filename = input("Enter the file name: ")
+    # Opens the file and handle exceptions
     try:
         f = open(filename, "r")
     except:
         print("Can't open", filename)
         return
+
+    # Makes sure each variable has reasonable minimum value
     num_words = 0
     num_nonwhite_chrs = 0
     num_alphanumeric_chrs = 0
 
+    # Reads the file line by line
     for line in f:
+        # Splits the line into words
         words = line.strip().split(' ')
-        if words[0] != '':
+        # Makes sure this is not a empty line
+        if words != ['']:
+            # Updates the number of words
             num_words += len(words)
-        nonwhite_chrs = str.join('', words)
-        num_nonwhite_chrs += len(nonwhite_chrs)
-        for c in nonwhite_chrs:
-            if (ord(c.lower()) >= ord('a') and ord(c.lower()) <= ord('z')) \
-               or (ord(c) >= ord('0') and ord(c) <= ord('9')):
-                num_alphanumeric_chrs += 1
+            # Joins the words into nonwhite characters (including punctuation)
+            nonwhite_chrs = str.join('', words)
+            # Updates the number of nonwhite characters (including punctuation)
+            num_nonwhite_chrs += len(nonwhite_chrs)
+            # Loops through characters in the nonwhite characters
+            # (including punctuation)
+            for c in nonwhite_chrs:
+                # Checks if the character is an alphanumeric character
+                if ((ord('a') <= ord(c.lower()) <= ord('z')) or
+                   (ord('0') <= ord(c) <= ord('9'))):
+                    # Updates the number of alphanumeric characters
+                    num_alphanumeric_chrs += 1
 
+    # Prints the results
     print("Words:", num_words)
     print("Characters: ", num_nonwhite_chrs)
     print("Letters & numbers:", num_alphanumeric_chrs)
