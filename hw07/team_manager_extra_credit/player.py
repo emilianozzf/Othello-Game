47f16ec4c1b25df780444f4ecb1ec0ea159e7565
Emiliano Zhu <zhu.em@husky.neu.edu>
2020-09-06 13:30:35 -0700

    Deleted unnecessary directories
    Deleted unrelated directories and only kept those related to Othello Game

--- hw07/team_manager_extra_credit/player.py
+++
@@ -1,6 +0,0 @@
-class Player:
-    """A class representing a dodgeball player"""
-    def __init__(self, name, number, position):
-        self.name = name
-        self.number = number
-        self.position = position
