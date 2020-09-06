c8f94ac0a75cd3ae1590c107ebea0fdcfd2bca9a
Emiliano Zhu <zhu.em@husky.neu.edu>
2020-09-06 13:39:12 -0700

    Deleted first version of Othello Game and modified the .gitignore file

--- hw11/othello_game/player2.py
+++
@@ -1,9 +0,0 @@
-class Player2:
-    """Player2 class"""
-    def __init__(self, board, game_controller):
-        self.board = board
-        self.gc = game_controller
-
-    def move(self, x, y):
-        """Handle keyboard input for Player2"""
-        self.board.put_tile(x, y, 1)
